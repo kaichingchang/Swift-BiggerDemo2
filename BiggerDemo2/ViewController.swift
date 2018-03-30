3aa8fd4703314e2470d1bbb8c54a08be645b906f
KaichingChang <kaichingc@gmail.com>
2018-03-30 19:26:44 +0800

    update

--- BiggerDemo2/ViewController.swift
+++ BiggerDemo2/ViewController.swift
@@ -1,15 +1,29 @@
 //
-//  ViewController.swift
-//  BiggerDemo2
-//
-//  Created by 張凱慶 on 2018/3/27.
-//  Copyright © 2018年 kaiching. All rights reserved.
+//《程式語言教學誌》的範例程式
+// http://kaiching.org/
+// 檔名：ViewController.swift
+// 功能：示範整數條件運算
+// 作者：張凱慶
 //
 
 import Cocoa
 
 class ViewController: NSViewController {
-
+    @IBOutlet weak var n1: NSTextField!
+    @IBOutlet weak var n2: NSTextField!
+    @IBOutlet weak var display: NSTextField!
+    
+    @IBAction func compare(_ sender: NSButton) {
+        let a = Int(n1.stringValue)!
+        let b = Int(n2.stringValue)!
+        if a == b {
+            display.stringValue = "兩者相等。"
+        }
+        else {
+            display.stringValue = String(a > b ? a : b) + "比較大。"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
